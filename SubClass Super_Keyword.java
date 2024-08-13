e586fc3c36f602a3a1a3b6f31eff8c792469f67c
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-08-13 19:25:37 +0530

    Add files via upload

---
+++ SubClass Super_Keyword.java
@@ -0,0 +1,22 @@
+public class SubClass  extends SuperKey
+{
+
+	int num = 110;
+    void printNumber()
+    {
+	System.out.println(num);
+    }
+    void printSupNumber()
+    {
+    	System.out.println(super.num);
+       }
+    
+    //.....................................................
+    
+    public static void main(String args[])
+    {
+	SubClass obj= new SubClass();
+	obj.printNumber();	
+	obj.printSupNumber();
+    }
+}
