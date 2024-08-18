32dc8c6d94cefc0a50b5ecd91436deb9591fdbd2
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-08-18 16:37:31 +0530

    Add files via upload

---
+++ ThrowExample ThrowExample.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class ThrowExample { 
+  void myMethod(int num)throws IOException, ClassNotFoundException{ 
+     if(num==1)
+        throw new IOException("IOException Occurred");
+     else
+        throw new ClassNotFoundException("ClassNotFoundException");
+  } 
+} 
